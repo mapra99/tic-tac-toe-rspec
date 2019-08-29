49e39b417299ee1b5f5a442145fb08bffcc67f84
Ben <benjisgarza@gmail.com>
2019-08-29 10:31:12 -0700

    Setup rspec and the first rspec test

--- spec/main-spec.rb
+++ spec/main-spec.rb
@@ -1,13 +1,16 @@
-require '/home/ben/GitProjects/Ruby/Tic-tac-toe/bin/main.rb'
+require "./lib/logic.rb"
 
-RSpec.describe win_check do
+
+RSpec.describe Game do
     describe "#win_check" do
+
       before do
         @game = Game.new
         @game.board.current_state = [["X","X","X"],["2,1","2,2","2,3"],["3,1","3,2","3,3"]]
       end
-      it "initializes game" do
-        expect(@game.win_check("X")).to eql(true)
+      
+      it "board" do
+        expect(@game.board.win_check("X")).to eql(true)
       end
     end
   end

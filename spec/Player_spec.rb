e4b65c83eb377e4c45e70100d903e50ed49f4337
Ben <benjisgarza@gmail.com>
2019-08-30 09:09:00 -0700

    Seperated test files, added negative tests, added test for board initial

---
+++ spec/Player_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/logic.rb"
+
+RSpec.describe Player do
+    it "checks if a player is being correctly created" do
+      pl = Player.new("X", "Ben")
+      expect(pl.name).to eql("Ben")
+      expect(pl.character).to eql("X")
+    end
+    it "checks that player is not nil" do
+        pl = Player.new("X", "Ben")
+        expect(pl).not_to eql(nil)
+        expect(pl.name).not_to eql(nil)
+        expect(pl.character).not_to eql(nil)
+      end
+  end
